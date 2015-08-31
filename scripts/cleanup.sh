aa084b89b2148b94f3af26329d83cd7908913ecc
yiting.huang <yiting.huang@gensee-inc.com>
2015-08-31 10:04:31 +0800

    Update

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -eux
 
-# Remove Ansible.
-yum -y remove ansible
-
 # Zero out the rest of the free space using dd, then delete the written file.
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
